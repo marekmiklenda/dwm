6c7e43ef923f8273ea6b4caf8f8ef80ff9c94b16
Marek Miklenda <marekmiklenda@gmail.com>
2023-05-10 18:36:26 +0200

    Added Flameshot keybinds Increased default font size

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=14" };
+static const char dmenufont[]       = "monospace:size=14";
 
 #define NorFG "#FFFFFF"
 #define NorBG "#111222"
@@ -65,6 +65,7 @@ static const char *appmenucmd[] = { "appmenu", "$HOME/.config/appmenu/", DMENU_A
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *dunstclose[] = { "dunstctl", "close-all", NULL };
 static const char *dunsthist[] = { "dunstctl", "history-pop", NULL };
+static const char *flameshot[] = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        	function        	argument */
@@ -107,6 +108,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_iacute,	8)
 	{ MODKEY,			XK_Escape,	spawn,			{.v = dunstclose } },
 	{ MODKEY|ShiftMask,		XK_Escape,	spawn,			{.v = dunsthist } },
+	{ MODKEY|ShiftMask,		XK_s,		spawn,			{.v = flameshot } },
+	{ 0,				XK_Print,	spawn,			{.v = flameshot } },
 };
 
 /* button definitions */
