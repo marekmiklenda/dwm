eea381bb33845860d272d853b4185a3f2d2b5b75
Marek Miklenda <marekmiklenda@gmail.com>
2023-05-11 13:30:29 +0200

    New font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char *fonts[]          = { "Ori:monospace:size=18", "monospace:size=14" };
+static const char dmenufont[]       = "Ori:monospace:size=18";
 
 #define NorFG "#FFFFFF"
 #define NorBG "#111222"
